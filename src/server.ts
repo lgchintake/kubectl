9960ca378994dea1f1a37c79d58df8c75172b533
Laxminarayan <laxminarayan.lexicon@gmail.com>
2022-01-20 00:12:30 +0530

    First upload

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import express from "express";
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec)
+
+const app = express();
+app.set('view engine', 'ejs');
+
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
+
+
+app.get("/", async (req, res) => {
+    res.render('index');
+});
+
+async function execute(command) {
+    const output = await exec(command);
+    return output.stdout.trim();
+};
+
+io.on('connection', (socket) => {
+    socket.on('list', async (msg, callback) => {
+        var data = await execute("kubectl get pods -n axcards-dev");
+        data = data.split('\n');
+        var finalData = [];
+        for (var count = 1; count < data.length; count++) {
+            finalData.push(data[count].split(/ +/));
+        }
+        callback({ data: finalData });
+    });
+
+    socket.on('delete', async (podname, callback) => {
+        var data = await execute("kubectl delete pod " + podname + " -n axcards-dev");
+        callback({ data: data });
+    });
+
+    socket.on('logs', async (podName, callback) => {
+        var serviceName = podName.substring(0, podName.lastIndexOf('-', podName.lastIndexOf('-') - 1));
+        var data = await execute("kubectl logs " + podName + " -c " + serviceName + " -n axcards-dev");
+        callback({ data: data });
+    })
+});
+
+const server = http.listen(7000, function () {
+    console.log("Listening: 7000");
+})
